f5c5e68209a691f856a1985b8bb2767841014578
Katariina Varanka <n7koka00@students.oamk.fi>
2021-08-12 18:55:45 +0300

    added script.js file

---
+++ script.js
@@ -0,0 +1,8 @@
+// Declaring variables
+const questionToggle = document.querySelector(".question");
+
+const toggleQuestion = () => {
+  console.log("kysymystä painettu.");
+};
+
+questionToggle.addEventListener("click", toggleQuestion);
