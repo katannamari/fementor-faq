e4734ace2e9861b57434c6e169f7de231d3e98a0
Katariina Varanka <n7koka00@students.oamk.fi>
2021-08-13 13:41:09 +0300

    clicking a question displays the answer.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Declaring variables
 const questions = document.querySelectorAll(".question");
 
-const toggleQuestion = () => {
-  console.log("kysymystä painettu.");
-};
-
 for (let i = 0; i < questions.length; i++) {
   const question = questions[i];
-  question.addEventListener("click", toggleQuestion);
+
+  question.addEventListener("click", (e) => {
+    const currentQuestion = e.currentTarget;
+    const currentAnswer = currentQuestion.nextElementSibling;
+
+    if (currentAnswer.classList.contains("answer-active")) {
+      currentAnswer.classList.remove("answer-active");
+      currentQuestion.classList.remove("question-active");
+    } else {
+      currentAnswer.classList.add("answer-active");
+      currentQuestion.classList.add("question-active");
+    }
+  });
 }
